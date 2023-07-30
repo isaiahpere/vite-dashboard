b6c0d07d9bea6b7cafea346f5b8feb805484c37d
Heber Pereira <isaiahpereira88@gmail.com>
2023-07-30 13:12:38 -0700

    create navbar skeleton

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,32 @@
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="company logo" />
+        <span>TechyAdmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" className="notification" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2&lazy=load"
+            alt=""
+            className="user"
+          />
+          <span>Joseph</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
