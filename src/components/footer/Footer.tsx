1b7d5d61c9d65b42bcdc2995494d0334a309851b
Heber Pereira <isaiahpereira88@gmail.com>
2023-07-30 13:24:45 -0700

    create footer section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span>TechyAdmin</span>
+      <span>@TechyAdmin All Rights Reserved</span>
+    </div>
+  );
 };
 
 export default Footer;
