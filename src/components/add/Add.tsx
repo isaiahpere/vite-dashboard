2181986a87fab1903055844068ea4a9126127fff
Heber Pereira <isaiahpereira88@gmail.com>
2023-08-05 00:34:13 -0700

    create form & modal and complete users component

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,45 @@
+import { FC, FormEvent } from "react";
+import "./Add.scss";
+import { GridColDef } from "@mui/x-data-grid";
+
+interface Iprops {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Add: FC<Iprops> = ({ setOpen, slug, columns }) => {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //add new item here
+    // axios.post('/api/${slug}s', {data})
+  };
+
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={handleClose}>
+          X
+        </span>
+        <h1>Add New {slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => (
+              <div className="item">
+                <label>{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -4,11 +4,13 @@ import DataTable from "../../components/dataTable/DataTable";
 import { userRows } from "../../data";
 
 import "./Users.scss";
+import { useState } from "react";
+import Add from "../../components/add/Add";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "avatar",
+    field: "img",
     headerName: "Avatar",
     width: 100,
     renderCell: (params) => {
@@ -22,46 +24,58 @@ const columns: GridColDef[] = [
     headerName: "First name",
     width: 150,
     editable: true,
+    type: "string",
   },
   {
     field: "lastName",
     headerName: "Last name",
     width: 150,
     editable: true,
+    type: "string",
   },
   {
     field: "email",
     headerName: "Email",
     width: 250,
     editable: true,
+    type: "string",
   },
   {
     field: "phone",
     headerName: "Phone",
     sortable: false,
     width: 200,
+    type: "number",
   },
   {
     field: "createdAt",
     headerName: "Created At",
     width: 130,
+    type: "string",
   },
   {
     field: "verified",
     headerName: "Verified",
-    type: "boolean",
     width: 150,
+    type: "boolean",
   },
 ];
 
 const Users = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={handleOpen}>Add New User</button>
       </div>
       <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add setOpen={setOpen} slug="users" columns={columns} />}
     </div>
   );
 };
